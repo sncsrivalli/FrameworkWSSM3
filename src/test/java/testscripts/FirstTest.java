a4aa485028a2af10a81127a7425b181e329cebb5
QPS-Basavanagudi <QPS-Basavanagudi@DESKTOP-F52EDBA>
2023-01-31 11:29:22 +0530

    ***

--- src/test/java/testscripts/FirstTest.java
+++ src/test/java/testscripts/FirstTest.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import genericLibraries.BaseClass;
-
+//My First Test
 public class FirstTest extends BaseClass{
 
 	@Test
